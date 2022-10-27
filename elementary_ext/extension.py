e01509e099a445d411302afdc77ddb3dfb10bf7b
Stephane Burwash <stephane.burwash@potloc.com>
2022-10-27 17:40:59 -0400

    Added initialize command

--- elementary_ext/extension.py
+++ elementary_ext/extension.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 import os
+import shutil
 import pkgutil
 import subprocess
 import sys
@@ -12,6 +13,11 @@ import structlog
 from meltano.edk import models
 from meltano.edk.extension import ExtensionBase
 from meltano.edk.process import Invoker, log_subprocess_error
+
+try:
+    from importlib.resources import files as ir_files
+except ImportError:
+    from importlib_resources import files as ir_files
 
 log = structlog.get_logger()
 
@@ -105,4 +111,47 @@ class elementary(ExtensionBase):
                     name="elementary_invoker", description="pass through invoker"
                 ),
             ]
+        )
+
+    def initialize(self, force: bool = False) -> None:
+        """Initialize the extension.
+        Args:
+            force: Whether to force initialization.
+        """
+        if not self.dbt_project_dir.exists():
+            log.info("creating dbt project directory", path=self.dbt_project_dir)
+            self.dbt_project_dir.mkdir(parents=True, exist_ok=True)
+
+        for entry in ir_files("files_dbt_ext.bundle.transform").iterdir():
+            if entry.name == "__pycache__":
+                continue
+            log.debug(f"deploying {entry.name}", entry=entry, dst=self.dbt_project_dir)
+            if entry.is_file():
+                shutil.copy(entry, self.dbt_project_dir / entry.name)
+            else:
+                shutil.copytree(
+                    entry, self.dbt_project_dir / entry.name, dirs_exist_ok=True
+                )
+
+        if not self.dbt_profiles_dir.exists():
+            log.info("creating dbt profiles directory", path=self.dbt_profiles_dir)
+            self.dbt_profiles_dir.mkdir(parents=True, exist_ok=True)
+
+        for entry in ir_files("files_dbt_ext.profiles").iterdir():
+            if entry.name == self.dbt_ext_type and entry.is_dir():
+                log.debug(
+                    f"deploying {entry.name} profile",
+                    entry=entry,
+                    dst=self.dbt_profiles_dir,
+                )
+                shutil.copytree(entry, self.dbt_profiles_dir, dirs_exist_ok=True)
+                break
+        else:
+            log.error(f"dbt type {self.dbt_ext_type} had no matching profile.")
+
+        log.info(
+            "dbt initialized",
+            dbt_ext_type=self.dbt_ext_type,
+            dbt_project_dir=self.dbt_project_dir,
+            dbt_profiles_dir=self.dbt_profiles_dir,
         )
