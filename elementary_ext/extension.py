f73dfbf920282cfc4d16293d6ce1b2957133e908
Stephane Burwash <stephane.burwash@potloc.com>
2023-01-25 13:19:08 -0500

    String to tuple

--- elementary_ext/extension.py
+++ elementary_ext/extension.py
@@ -97,9 +97,9 @@ class elementary(ExtensionBase):
             log.info(f"Extension executing `{command_msg}`...")
 
             if self.config_dir_path != "":
-                command_args = command_args + ("--config-dir-path=" + str(self.config_dir_path))
+                command_args = command_args + ("--config-dir-path=" + str(self.config_dir_path),)
             elif self.dbt_profiles_dir != "":
-                command_args = command_args + ("--profiles-dir=" + str(self.dbt_profiles_dir))
+                command_args = command_args + ("--profiles-dir=" + str(self.dbt_profiles_dir),)
 
             self.elementary_invoker.run_and_log(command_name, *command_args)
         except subprocess.CalledProcessError as err:
