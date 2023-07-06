fdae7c996c96c1b448c95bfd6db999bcee8f7ad7
Stéphane Burwash <sburwash@live.ca>
2023-07-06 17:10:12 -0400

    Edited command args

--- elementary_ext/extension.py
+++ elementary_ext/extension.py
@@ -110,25 +110,24 @@ class elementary(ExtensionBase):
                     log.info(f"Using config at `{self.config_dir_path}`...")
                 elif self.dbt_profiles_dir != "":
                     command_args = command_args + ("--profiles-dir=" + str(self.dbt_profiles_dir),)
-                    log.info(f"Using profile.yml at `{self.dbt_profiles_dir}`...")
-                    
+                    log.info(f"Using profile.yml at `{self.dbt_profiles_dir}`...")          
                 if self.days_back is not None:
-                    commands_args = commands_args + ("--days-back" + str(self.days_back),)
+                    command_args = command_args + ("--days-back" + str(self.days_back),)
                     log.info(f"Using days_back `{self.days_back}`...")
                 if self.timezone is not None:
-                    commands_args = commands_args + ("--timezone" + str(self.timezone),)
+                    command_args = command_args + ("--timezone" + str(self.timezone),)
                     log.info(f"Using timezone `{self.timezone}`...")
                 if self.dbt_quoting is not None:
-                    commands_args = commands_args + ("--dbt-quoting" + str(self.dbt_quoting),)
+                    command_args = command_args + ("--dbt-quoting" + str(self.dbt_quoting),)
                     log.info(f"Using dbt_quoting `{self.dbt_quoting}`...")
                 if self.disable_samples is not None:
-                    commands_args = commands_args + ("--disable-samples" + str(self.disable_samples),)
+                    command_args = command_args + ("--disable-samples" + str(self.disable_samples),)
                     log.info(f"Using disable_samples `{self.disable_samples}`...")
                 if self.environment is not None:
-                    commands_args = commands_args + ("--env" + str(self.environment),)
+                    command_args = command_args + ("--env" + str(self.environment),)
                     log.info(f"Using environment `{self.environment}`...")
                 if self.full_refresh_dbt_models is not None:
-                    commands_args = commands_args + ("--full-refresh-dbt-package" + str(self.full_refresh_dbt_models),)
+                    command_args = command_args + ("--full-refresh-dbt-package" + str(self.full_refresh_dbt_models),)
                     log.info(f"Using full_refresh_dbt_models `{self.environment}`...")
                     
             self.elementary_invoker.run_and_log(command_name, *command_args)
